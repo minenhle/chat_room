3adf59d53a6c23c850b877e9702a825f032a2ccd
minenhle <minenhle@gmail.com>
2013-07-22 11:38:18 +0200

    Basic Socket.io chat application

---
+++ server.js
@@ -0,0 +1,6 @@
+#!/usr/bin/env node
+var io = require('socket.io'),
+    connect = require('connect');
+    
+    var app = connect().use(connect.static('public')).listen(3000);
+    var socket = io.listen(app);
